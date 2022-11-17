c298aba87f86e34d39d3891a4bb829b375428297
Samuel Effler <samuel.effler@student.uni-tuebingen.de>
2022-11-17 15:35:01 +0100

    add mesh generation and height visualization, tutorial ep. 05

--- 3D Generator/Assets/Scripts/MapGenerator.cs
+++ 3D Generator/Assets/Scripts/MapGenerator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    public enum DrawMode {NoiseMap, ColorMap};
+    public enum DrawMode {NoiseMap, ColorMap, Mesh};
     public DrawMode drawMode;
 
     public int mapWidth;
@@ -18,6 +18,9 @@ public class MapGenerator : MonoBehaviour
 
     public int seed;
     public Vector2 offset;
+
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
     
     public bool autoUpdate;
 
@@ -52,6 +55,10 @@ public class MapGenerator : MonoBehaviour
         else if (drawMode == DrawMode.ColorMap)
         {
             display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+        }
+        else if (drawMode == DrawMode.Mesh)
+        {
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve), TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
         }
     }
  
